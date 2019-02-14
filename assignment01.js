70aad796f8aa9809a6aa7523866c31f3c325c2a4
jalpendesai <djalpen24@gmail.com>
2019-02-14 18:31:59 -0500

    Implemented Basic Planet and Moon Rotation

--- assignment01.js
+++ assignment01.js
@@ -1,42 +1,55 @@
+
 var camera,
-    renderer,
-    scene,
-    trackballControl,
-    clock
-    ;
+renderer,
+scene,
+trackballControl,
+clock
+;
+
+var planet1, planet2,planet3, moonTest1,moonTest2;
+var geoPlanet, matPlanet;
 
 var ambientLight;
 
 //  Initialization
 function init(){
-    scene       = new THREE.Scene();
-    renderer    = new THREE.WebGLRenderer();
+scene       = new THREE.Scene();
+renderer    = new THREE.WebGLRenderer();
 
-    renderer.setSize(window.innerHeight, window.innerWidth);
-    document.body.appendChild(renderer.domElement);
-    renderer.shadowMap.enabled = true;
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xbfbfbf); // Set it to 0x050505
+document.body.appendChild(renderer.domElement);
 
-    clock       =  new THREE.Clock();
+clock = new THREE.Clock();
 }
 
 //  Camera and Light setup
 function setUpCameraAndLight(){
-    camera  = new THREE.PerspectiveCamera(
-        50, 
-        window.innerWidth/window.innerHeight,
-        100);
-    
-    camera.position.x   = 15;
-    camera.position.y   = 10;
-    camera.position.z   = 10;
-    camera.lookAt(scene.position);
+camera  = new THREE.PerspectiveCamera(
+    80, 
+    window.innerWidth/window.innerHeight,
+    0.1,
+    1000);
 
-    trackballControl = new THREE.TrackballControls(camera,render.domElement);
+camera.position.x   = 0;
+camera.position.y   = -60;
+camera.position.z   = 0;
 
-    //  Light
-    // ambientLight            = new THREE.AmbientLight({color:0x3c3c3c});
-    // ambientLight.castShadow = true;
-    // scene.add(ambientLight);
+camera.lookAt(scene.position);
+
+// Lights
+   
+ambient = new THREE.AmbientLight(0xfcfc00,0.02);
+ambient.castShadow = true;
+scene.add(ambient);
+
+var pointLight = new THREE.PointLight(0xfcfc00,10,100);
+pointLight.position.set(0,0,0);
+pointLight.castShadow = true;
+scene.add(pointLight);
+
+
+trackballControl = new THREE.TrackballControls(camera, renderer.domElement);
 }
 
 // Custom Inspectper Setting up
@@ -46,28 +59,73 @@ function customInspector(){
 
 //  Creating planet Geometry
 function createGeometry(){
-    
-    //  SUN
-    let _geoSUN = new THREE.SphereGeometry(30);
-    let _matSUN = new THREE.MeshLambertMaterial ({color : 0xffff00});
-    let sun     = new THREE.Mesh(_geoSUN,_matSUN);
 
-    sun.position.set(10,10,10);
-    
-    scene.add(sun);
+//  SUN
+let _geoSUN = new THREE.SphereGeometry(4,32,32);
+let _matSUN = new THREE.MeshPhongMaterial ({color : 0xfcfc00, emissive: 0xfcfc00, emissiveIntensity:5});
+let sun     = new THREE.Mesh(_geoSUN,_matSUN);
+
+//sun.position.set(0,-5,5);
+
+sun.castShadow = true;
+sun.receiveShadow = true;
+scene.add(sun);
+
+
+// Planet2
+let _geoPlanet2 = new THREE.SphereGeometry(2,32,32);
+let _matPlanet2 = new THREE.MeshPhongMaterial({color:0x0061ff});
+planet2 = new THREE.Mesh(_geoPlanet2,_matPlanet2);
+
+planet2.position.set(30,0,0);
+planet2.castShadow = true;
+planet2.receiveShadow = true;
+
+
+planet1 = new THREE.Object3D();
+addPlanet(planet1,3,0x0061ff,20);
+
+planet3 = new THREE.Object3D();
+addPlanet(planet3,2,0xff512d,30);
+
+moonTest1 = new THREE.Object3D();
+moonTest1.position.set(20,0,0);
+addPlanet(moonTest1,0.5,0x2e69ff,5);
+planet1.add(moonTest1);
+
+}
+
+function addPlanet(obj, planet_Size, planet_Color, distanceFromSun ){
+    let geoPlanet = new THREE.SphereGeometry(planet_Size,32,32);
+    let matPlanet = new THREE.MeshLambertMaterial({color: planet_Color});
+    let planet = new THREE.Mesh(geoPlanet, matPlanet);
+    planet.position.set(distanceFromSun,0,0);
+    planet.castShadow = true;
+    planet.receiveShadow = true;
+
+    obj.add(planet);
+    scene.add(obj);
+
 }
 
 function render(){
-    trackballControl.update(clock.getDelta());
-    renderer.render(scene,camera);
+// planetRotation();
+trackballControl.update(clock.getDelta());
+renderer.render(scene,camera);
 
-    requestAnimationFrame(render);
+planet1.rotation.z += 0.01;
+planet3.rotation.z += 0.02;
+moonTest1.rotation.y += 0.01;
+requestAnimationFrame(render);
 }
 
+
+
 window.onload = () => {
-    init();
-    setUpCameraAndLight();
-    createGeometry();
-    customInspector();
-    render();
+init();
+
+createGeometry();
+customInspector();
+setUpCameraAndLight();
+render();
 }
